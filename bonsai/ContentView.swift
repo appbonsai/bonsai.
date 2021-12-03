fd107e32d19b7dd80be80805c82b4dfa608cc820
Maksim Alekseev <mrvalter566@gmail.com>
2021-12-03 19:21:39 +0700

    Initial commit

---
+++ bonsai/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  bonsai
+//
+//  Created by Максим Алексеев  on 03.12.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ bonsai/bonsaiApp.swift
@@ -0,0 +1,17 @@
+//
+//  bonsaiApp.swift
+//  bonsai
+//
+//  Created by Максим Алексеев  on 03.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct bonsaiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
