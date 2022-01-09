56b0b5629378500f7ff04823e02c9679ec4f134e
antonhoang <62542442+antonhoang@users.noreply.github.com>
2022-01-09 13:46:24 +0200

    [BS-14] create budget transaction UI (#10)
    Add budget transaction screen
    Pass Transaction with DataController context
    
    Co-authored-by: antonhoang <hoang@hoangs-iMac.local>

---
+++ bonsai/Mocks/MockDataTransactions.swift
@@ -0,0 +1,71 @@
+//
+//  MockDataTransactions.swift
+//  bonsai
+//
+//  Created by hoang on 18.12.2021.
+//
+
+import Foundation
+
+public struct MockDataTransaction {
+   public static let viewContext = DataController.sharedInstance.container.viewContext
+
+   public static let transactions = [
+      Transaction(context: Self.viewContext,
+                  amount: 20.0,
+                  date: .from(year: 2021, month: 12, day: 17),
+                  category: .init(
+                     context: viewContext,
+                     title: "Restaurant",
+                     color: .green,
+                     icon: BonsaiImage.forkKnifeCircle),
+                  account: .init(
+                     context: viewContext,
+                     title: "BonsaiTest"),
+                  type: .expense,
+                  tags: .init()),
+
+      Transaction(context: Self.viewContext,
+                  amount: 693.5,
+                  date: .from(year: 2021, month: 12, day: 17),
+                  category: .init(
+                     context: viewContext,
+                     title: "Fuel",
+                     color: .blue,
+                     icon: BonsaiImage.fuelpump),
+                  account: .init(
+                     context: viewContext,
+                     title: "BonsaiTest"),
+                  type: .expense,
+                  tags: .init()),
+
+      Transaction(context: Self.viewContext,
+                  amount: 776602.0,
+                  date: .from(year: 2021, month: 8, day: 22),
+                  category: .init(
+                     context: viewContext,
+                     title: "Graduation",
+                     color: .red,
+                     icon: BonsaiImage.graduationcapCircle),
+                  account: .init(
+                     context: viewContext,
+                     title: "BonsaiTest"),
+                  type: .expense,
+                  tags: .init()),
+
+      Transaction(context: Self.viewContext,
+                  amount: 40.0,
+                  date: .from(year: 2021, month: 12, day: 18),
+                  category: .init(
+                     context: viewContext,
+                     title: "Salary",
+                     color: .red,
+                     icon: BonsaiImage.amount),
+                  account: .init(
+                     context: viewContext,
+                     title: "BonsaiTest"),
+                  type: .income,
+                  tags: .init())
+   ]
+}
+
