72a590765e64a2b1227550f5a583b4621544cd40
VladimirBrejcha <vladimirbrejcha@icloud.com>
2022-01-16 03:41:11 +0300

    Appearance folder -> Resources folder; move Assets to resources; add Icons

---
+++ bonsai/Resources/Icons.swift
@@ -0,0 +1,42 @@
+//
+//  Icons.swift
+//  bonsai
+//
+//  Created by Vladimir Korolev on 16.01.2022.
+//
+
+import SwiftUI
+
+enum Icons {
+   static var airplane: Image { .init(systemName: "airplane.departure") }
+   static var car: Image { .init(systemName: "car.fill") }
+   static var bus: Image { .init(systemName: "bus.fill") }
+   static var tram: Image { .init(systemName: "tram.fill") }
+   static var ferry: Image { .init(systemName: "ferry.fill") }
+   static var bicycle: Image { .init(systemName: "bicycle") }
+   static var fuel: Image { .init(systemName: "fuelpump.fill") }
+   static var person: Image { .init(systemName: "person.fill") }
+   static var persons: Image { .init(systemName: "person.2.fill") }
+   static var tShirt: Image { .init(systemName: "tshirt.fill") }
+   static var drop: Image { .init(systemName: "drop.fill") }
+   static var pawPrint: Image { .init(systemName: "pawprint.fill") }
+   static var leaf: Image { .init(systemName: "leaf.fill") }
+   static var bag: Image { .init(systemName: "bag.fill") }
+   static var cart: Image { .init(systemName: "cart.fill") }
+   static var creditCard: Image { .init(systemName: "creditcard.fill") }
+   static var heart: Image { .init(systemName: "heart.fill") }
+   static var crossCase: Image { .init(systemName: "cross.case.fill") }
+   static var pills: Image { .init(systemName: "pills.fill") }
+   static var phone: Image { .init(systemName: "phone.fill") }
+   static var books: Image { .init(systemName: "books.vertical.fill") }
+   static var graduationCap: Image { .init(systemName: "graduationcap.fill") }
+   static var ticket: Image { .init(systemName: "ticket.fill") }
+   static var hammer: Image { .init(systemName: "hammer.fill") }
+   static var briefCase: Image { .init(systemName: "briefcase.fill") }
+   static var theaterMasks: Image { .init(systemName: "theatermasks.fill") }
+   static var key: Image { .init(systemName: "key.fill") }
+   static var gameController: Image { .init(systemName: "gamecontroller.fill") }
+   static var paintPalette: Image { .init(systemName: "paintpalette.fill") }
+   static var gift: Image { .init(systemName: "gift.fill") }
+   static var display: Image { .init(systemName: "display") }
+}
