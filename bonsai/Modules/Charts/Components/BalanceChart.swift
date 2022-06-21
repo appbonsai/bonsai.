652fb66fb7c8881eee11faac007087592cb0e5c9
Maksim Alekseev <mrvalter566@gmail.com>
2022-06-21 14:49:39 +0700

    Add new dependency - SwiftUICharts

---
+++ bonsai/Modules/Charts/Components/BalanceChart.swift
@@ -0,0 +1,20 @@
+//
+//  BalanceChart.swift
+//  bonsai
+//
+//  Created by Максим Алексеев  on 21.06.2022.
+//
+
+import SwiftUI
+
+struct BalanceChart: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BalanceChart_Previews: PreviewProvider {
+    static var previews: some View {
+        BalanceChart()
+    }
+}
