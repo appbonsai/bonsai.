7dde3ad6604bc93a2b6b2613af296ef559e51d4f
antuan.khoanh <antonhoang@Antons-MacBook-Pro.local>
2022-06-27 07:58:58 +0200

    add update/ delete tests

--- BudgetServiceTests/BudgetServiceTests.swift
+++ BudgetServiceTests/BudgetServiceTests.swift
@@ -43,6 +43,34 @@ class BudgetServiceTests: XCTestCase {
         XCTAssertNotNil(budget)
     }
     
+    func testUpdateBudget() throws {
+        // g
+        let sut = makeSUT()
+        // w
+        let oldBudget = try sut.create(name: "na Tailand", amount: 9999, periodDays: 45)
+        oldBudget.name = "na Bali"
+        oldBudget.amount = 8888
+        oldBudget.periodDays = 17
+        try sut.update(budget: oldBudget)
+        // t
+        XCTAssertEqual(oldBudget.name, "na Bali")
+        XCTAssertEqual(oldBudget.amount, 8888)
+        XCTAssertEqual(oldBudget.periodDays, 17)
+    }
+    
+    func testDeleteBudget() throws {
+        // g
+        let sut = makeSUT()
+        // w
+        try sut.create(name: "na Tailand", amount: 9999, periodDays: 45)
+        try sut.delete()
+        let action = { try sut.getBudget() }
+        // t
+        XCTAssertThrowsError(try action()) { error in
+            XCTAssertTrue(error is BudgetService.BudgetDoesntExist)
+        }
+    }
+    
     func makeSUT() -> BudgetService {
         let dataController = DataControllerMock()
         return BudgetService(context: dataController.mainContext)
@@ -78,5 +106,21 @@ class BudgetService {
         return budget
     }
     
+    func update(budget: Budget) throws {
+        let request: NSFetchRequest<Budget> = Budget.fetchRequest()
+        request.fetchLimit = 1
+        let currentBudget = try getBudget()
+        currentBudget.setValue(budget.name, forKeyPath: #keyPath(Budget.name))
+        currentBudget.setValue(budget.amount, forKeyPath: #keyPath(Budget.amount))
+        currentBudget.setValue(budget.periodDays, forKeyPath: #keyPath(Budget.periodDays))
+    }
+    
+    func delete() throws {
+        let currentBudget = try getBudget()
+        context.delete(currentBudget)
+        try context.save()
+    }
+    
     struct BudgetDoesntExist: Error { }
+    
 }
