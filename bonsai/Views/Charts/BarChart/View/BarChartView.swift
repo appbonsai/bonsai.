8054ad42098903ebef58e6a0d2bda8a129614103
Maksim Alekseev <mrvalter566@gmail.com>
2022-07-10 12:16:19 +0700

    Add functionality to present detail labels when touching bar

--- bonsai/Views/Charts/BarChart/View/BarChartView.swift
+++ bonsai/Views/Charts/BarChart/View/BarChartView.swift
@@ -43,9 +43,16 @@ struct BarChartView: View {
                 // bars
                 GeometryReader { geo in
                     VStack {
+                        if !currentLabel.isEmpty {
+                            BarDetailLabel(title: currentValue, color: viewModel.chartData.piecesColor)
+                                .offset(x: labelOffset(in: geo.frame(in: .local).width))
+                                .animation(.easeIn)
+                        }
+                        
                         HStack(spacing: 12) {
                             ForEach(Array(viewModel.chartData.pieces.enumerated()), id: \.offset) { i, bar in
                                 VStack {
+                                    
                                     BarChartLineView(value: viewModel.normalizedValue(value: bar.value), color: viewModel.chartData.piecesColor)
                                         .scaleEffect(barIsTouched(index: i) ? CGSize(width: 1.3, height: 1.1) : CGSize(width: 1, height: 1), anchor: .bottom)
                                         .shadow(color: viewModel.chartData.piecesColor.opacity(barIsTouched(index: i) ? 0.5 : 0), radius: 15, x: 0, y: 0)
@@ -61,11 +68,14 @@ struct BarChartView: View {
                             DragGesture(minimumDistance: 0)
                                 .onChanged { position in
                                     touchLocation = position.location.x/geo.frame(in: .local).width
+                                    updateCurrentValue()
                                 }
                                 .onEnded { position in
-                                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
                                         withAnimation(Animation.easeOut(duration: 0.5)) {
                                             touchLocation = -1
+                                            currentValue  =  ""
+                                            currentLabel = ""
                                         }
                                     }
                                 }
@@ -92,6 +102,27 @@ struct BarChartView: View {
     /// Check if touch location is greater than current bar's beggining and less that next bar's beggining
     func barIsTouched(index: Int) -> Bool {
         touchLocation > CGFloat(index)/CGFloat(viewModel.chartData.pieces.count) && touchLocation < CGFloat(index+1)/CGFloat(viewModel.chartData.pieces.count)
+    }
+    
+    func updateCurrentValue()    {
+        let index = Int(touchLocation * CGFloat(viewModel.chartData.pieces.count))
+        guard index < viewModel.chartData.pieces.count && index >= 0 else {
+            currentValue = ""
+            currentLabel = ""
+            return
+        }
+        
+        currentValue = "\(viewModel.chartData.pieces[index].value)"
+        currentLabel = viewModel.chartData.pieces[index].label
+    }
+    
+    func labelOffset(in width: CGFloat) -> CGFloat {
+        let currentIndex = Int(touchLocation * CGFloat(viewModel.chartData.pieces.count))
+        guard currentIndex < viewModel.chartData.pieces.count && currentIndex >= 0 else { return 0 }
+        let cellWidth = width / CGFloat(viewModel.chartData.pieces.count)
+        let actualWidth = width - cellWidth
+        let position = cellWidth * CGFloat(currentIndex) - actualWidth / 2
+        return position
     }
 }
 
