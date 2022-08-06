2a9b12c55478a7baef54674e2760f84a9ba02d5c
antuan.khoanh <antonhoang@Antons-MacBook-Pro.local>
2022-08-06 21:15:53 +0200

    fix color issues

--- bonsai/Modules/Subscriptions/Subscriptions.swift
+++ bonsai/Modules/Subscriptions/Subscriptions.swift
@@ -12,87 +12,99 @@ struct Subscriptions: View {
     @State var isSelected: Bool = false
     
     var body: some View {
-        List {
-            HStack {
-                Image("undraw_japan_ubgk 1")
-                    .resizable()
-                    .scaledToFill()
-                    .clipped()
-                    .padding([.leading, .trailing], 73)
-            }
-            .listRowSeparator(.hidden)
-            .padding(.bottom, 12)
-            .padding(.top, 40)
-            HStack(alignment: .center) {
-                Spacer()
-                Text("Choose your plan")
-                    .font(.system(size: 28))
-                    .bold()
-                    .foregroundColor(BonsaiColor.purple7)
-                Spacer()
-            }
-            .padding(.bottom, 12)
-            ForEach(0..<4) { index in
-                /*
-                 product subscription
-                 */
-                SubscriptionCell()
-                    .padding(.bottom, 6)
-                    .listRowSeparator(.hidden)
-                    .onTapGesture {
-                        
-                        /*
-                         product subscription
-                         update checkmark
-                         unselect another
-                         */
-                    }
-            }
-            
-            Group {
-                Text("By subscribing you agree to our ")
-                +
-                Text("Terms of Service ").foregroundColor(BonsaiColor.secondary) +
-                Text("and ") +
-                Text("Privacy Policy").foregroundColor(BonsaiColor.secondary) +
-                Text(".")
-            }
-            .font(.system(size: 12))
-            .lineLimit(3)
-            .listRowSeparator(.hidden)
-            
-            Button {
-                
-            } label: {
+        ZStack {
+            BonsaiColor.back
+                .ignoresSafeArea()
+            List {
+                HStack {
+                    Image("undraw_japan_ubgk 1")
+                        .resizable()
+                        .scaledToFill()
+                        .clipped()
+                        .padding([.leading, .trailing], 73)
+                }
+                .listRowBackground(BonsaiColor.back)
+                .listRowSeparator(.hidden)
+                .padding(.bottom, 12)
+                .padding(.top, 40)
                 HStack(alignment: .center) {
-                    Text("Restore Purchases")
-                        .font(.system(size: 12))
-                        .foregroundColor(BonsaiColor.secondary)
+                    Spacer()
+                    Text("Choose your plan")
+                        .font(.system(size: 28))
+                        .bold()
+                        .foregroundColor(BonsaiColor.purple7)
                     Spacer()
                 }
-            }
-            .listRowSeparator(.hidden)
-            
-            HStack(alignment: .center) {
-                Spacer()
-                ZStack {
-                    RoundedRectangle(cornerRadius: 13)
-                        .frame(width: 192, height: 48)
-                        .foregroundColor(BonsaiColor.mainPurple)
-                    Text("Get all feutures")
-                        .foregroundColor(BonsaiColor.card)
-                        .font(.system(size: 17))
-                        .bold()
+                .listRowBackground(BonsaiColor.back)
+                .padding(.bottom, 12)
+                ForEach(0..<4) { index in
+                    /*
+                     product subscription
+                     */
+                    SubscriptionCell()
+                        .padding(.bottom, 6)
+                        .listRowSeparator(.hidden)
+                        .onTapGesture {
+                            
+                            /*
+                             product subscription
+                             update checkmark
+                             unselect another
+                             */
+                        }
                 }
-                Spacer()
+                .listRowBackground(BonsaiColor.back)
+                
+                Group {
+                    Text("By subscribing you agree to our ")
+                    +
+                    Text("Terms of Service ").foregroundColor(BonsaiColor.secondary) +
+                    Text("and ") +
+                    Text("Privacy Policy").foregroundColor(BonsaiColor.secondary) +
+                    Text(".")
+                }
+                .font(.system(size: 12))
+                .lineLimit(3)
+                .listRowSeparator(.hidden)
+                .listRowBackground(BonsaiColor.back)
+                
+                Button {
+                    
+                } label: {
+                    HStack(alignment: .center) {
+                        Text("Restore Purchases")
+                            .font(.system(size: 12))
+                            .foregroundColor(BonsaiColor.secondary)
+                        Spacer()
+                    }
+                }
+                .listRowSeparator(.hidden)
+                .listRowBackground(BonsaiColor.back)
+                
+                HStack(alignment: .center) {
+                    Spacer()
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 13)
+                            .frame(width: 192, height: 48)
+                            .foregroundColor(BonsaiColor.mainPurple)
+                        Text("Get all feutures")
+                            .foregroundColor(BonsaiColor.card)
+                            .font(.system(size: 17))
+                            .bold()
+                    }
+                    Spacer()
+                }
+                .listRowSeparator(.hidden)
+                .listRowBackground(BonsaiColor.back)
+                .padding(.top, 17)
+                .padding(.bottom, 50)
             }
-            .padding(.top, 17)
-            .padding(.bottom, 50)
+            .onAppear {
+                UITableView.appearance().showsVerticalScrollIndicator = false
+            }
+            .listStyle(PlainListStyle())
+            .ignoresSafeArea()
         }
-        .listStyle(PlainListStyle())
-        .listRowBackground(Color.clear)
-        .ignoresSafeArea()
-        .edgesIgnoringSafeArea(.all)
     }
 }
 
