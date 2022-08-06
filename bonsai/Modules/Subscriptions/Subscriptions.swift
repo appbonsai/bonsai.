f2893fa6874541a73e433fec6dbc8089128cc6e8
antuan.khoanh <antonhoang@Antons-MacBook-Pro.local>
2022-08-06 18:20:54 +0200

    create list for subscriptions

--- bonsai/Modules/Subscriptions/Subscriptions.swift
+++ bonsai/Modules/Subscriptions/Subscriptions.swift
@@ -10,8 +10,38 @@ import SwiftUI
 struct Subscriptions: View {
     var body: some View {
         
-        ForEach(0..<4) { index in 
-            SubscriptionCell()
+        List {
+            HStack(alignment: .center) {
+                Spacer()
+                Text("Choose your plan")
+                Spacer()
+            }
+            ForEach(0..<4) { index in
+                SubscriptionCell()
+            }
+            
+            Text("By subscribing you agree to our Terms of Service and Privacy Policy.")
+            
+            Button {
+                
+            } label: {
+                Text("Restore Purchases")
+            }
+
+            
+            HStack(alignment: .center) {
+                Spacer()
+                ZStack {
+                    RoundedRectangle(cornerRadius: 13)
+                        .frame(width: 192, height: 48)
+                        .foregroundColor(BonsaiColor.mainPurple)
+                    Text("Get all feutures")
+                        .foregroundColor(BonsaiColor.card)
+                        .font(.system(size: 17))
+                        .bold()
+                }
+                Spacer()
+            }
         }
     }
 }
