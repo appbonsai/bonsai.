f5b883b79a3079c78c0c10847deb6a9f39845382
antuan.khoanh <antonhoang@Antons-MacBook-Pro.local>
2022-08-07 22:59:56 +0200

    add some animations

--- bonsai/Modules/Onboarding/Onboarding.swift
+++ bonsai/Modules/Onboarding/Onboarding.swift
@@ -17,40 +17,49 @@ struct Onboarding: View {
         "bonsai_3"
     ]
     
+    @State var fadein: Bool = false
+    
     var body: some View {
-        ZStack {
-            VStack {
-                TabView(selection: $selection) {
-                    ForEach(0..<3) { index in
-                        OnboardingCard(image: onboardingImages[index])
-                            .tag(index)
-                    }
+        VStack {
+            TabView(selection: $selection) {
+                ForEach(0..<onboardingImages.count, id: \.self) { index in
+                    OnboardingCard(image: onboardingImages[index])
+                        .tag(index)
                 }
-                Button {
-                    if selection != onboardingImages.count - 1 {
-                        selection += 1
-                    } else {
-                        // open MAIN SCREEN
-                    }
-                } label: {
-                    ZStack {
-                        RoundedRectangle(cornerRadius: 13)
-                            .frame(width: 192, height: 48)
-                            .foregroundColor(BonsaiColor.mainPurple)
-                            Text("Continue")
-                            .foregroundColor(BonsaiColor.card)
-                            .font(.system(size: 17))
-                            .bold()
-                    }
-                }
-                .padding(.bottom, 74)
-
             }
-            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .always))
-            .animation(.easeInOut)
-            .ignoresSafeArea()
+            Button {
+                if selection != onboardingImages.count - 1 {
+                    withAnimation {
+                        selection += 1
+                    }
+                    withAnimation(.easeInOut(duration: 0.3)) {
+                        fadein.toggle()
+                    }
+                    withAnimation(.easeInOut(duration: 0.6)) {
+                        fadein.toggle()
+                    }
+                } else {
+                    // open MAIN SCREEN
+                }
+            } label: {
+                ZStack {
+                    RoundedRectangle(cornerRadius: 13)
+                        .frame(width: 192, height: 48)
+                        .foregroundColor(BonsaiColor.mainPurple)
+                    Text("Continue")
+                        .foregroundColor(BonsaiColor.card)
+                        .font(.system(size: 17))
+                        .bold()
+                }
+            }
+            .padding(.bottom, 74)
+            
         }
+        .opacity(fadein ? 0.1 : 1.0)
+        .tabViewStyle(PageTabViewStyle(indexDisplayMode: .always))
+        .ignoresSafeArea()
     }
+    
 }
 
 struct Onboarding_Previews: PreviewProvider {
