03e6b467ebdcc0d10517125b61b7175012fac45c
antonhoang <62542442+antonhoang@users.noreply.github.com>
2022-09-03 21:29:15 +0200

    add linkable text for Terms of Service and Privacy Policy (#76)
    Co-authored-by: antuan.khoanh <antonhoang@Antons-MacBook-Pro.local>

--- bonsai/Modules/Subscriptions/Subscriptions.swift
+++ bonsai/Modules/Subscriptions/Subscriptions.swift
@@ -8,102 +8,108 @@
 import SwiftUI
 
 struct Subscriptions: View {
-    
-    @State var id: String = MockSubscriptions.subscriptions.first(where: { $0.isMostPopular })?.id ?? ""
-
-    let mockSubs = MockSubscriptions.subscriptions
-    
-    var body: some View {
-        ZStack {
-            BonsaiColor.back
-                .ignoresSafeArea()
-            List {
-                HStack {
-                    Image("undraw_japan_ubgk 1")
-                        .resizable()
-                        .scaledToFill()
-                        .clipped()
-                        .padding([.leading, .trailing], 73)
-                }
-                .listRowBackground(BonsaiColor.back)
-                .listRowSeparator(.hidden)
-                .padding(.bottom, 12)
-                .padding(.top, 20)
-                HStack(alignment: .center) {
-                    Spacer()
-                    Text("Choose your plan")
-                        .font(.system(size: 28))
-                        .bold()
-                        .foregroundColor(BonsaiColor.purple7)
-                    Spacer()
-                }
-                .listRowBackground(BonsaiColor.back)
-                .padding(.bottom, 12)
-                ForEach(0..<4) { index in
-                    SubscriptionCell(subscription: mockSubs[index], id: id)
-                        .listRowSeparator(.hidden)
-                        .onTapGesture {
-                            let subscription = mockSubs[index]
-                            id = subscription.id
-                        }
-                }
-                .listRowBackground(BonsaiColor.back)
-                
-                Group {
-                    Text("By subscribing you agree to our ")
-                    +
-                    Text("Terms of Service ").foregroundColor(BonsaiColor.secondary) +
-                    Text("and ") +
-                    Text("Privacy Policy").foregroundColor(BonsaiColor.secondary) +
-                    Text(".")
-                }
-                .font(.system(size: 12))
-                .lineLimit(3)
-                .listRowSeparator(.hidden)
-                .listRowBackground(BonsaiColor.back)
-                
-                Button {
-                    
-                } label: {
-                    HStack(alignment: .center) {
-                        Text("Restore Purchases")
-                            .font(.system(size: 12))
-                            .foregroundColor(BonsaiColor.secondary)
-                        Spacer()
-                    }
-                }
-                .listRowSeparator(.hidden)
-                .listRowBackground(BonsaiColor.back)
-                
-                HStack(alignment: .center) {
-                    Spacer()
-                    ZStack {
-                        RoundedRectangle(cornerRadius: 13)
-                            .frame(width: 192, height: 48)
-                            .foregroundColor(BonsaiColor.mainPurple)
-                        Text("Get all features")
-                            .foregroundColor(BonsaiColor.card)
-                            .font(.system(size: 17))
-                            .bold()
-                    }
-                    Spacer()
-                }
-                .padding(.bottom, 30)
-                .listRowSeparator(.hidden)
-                .listRowBackground(BonsaiColor.back)
+   
+   @State var id: String = MockSubscriptions.subscriptions.first(where: { $0.isMostPopular })?.id ?? ""
+   
+   let mockSubs = MockSubscriptions.subscriptions
+   
+   var body: some View {
+      ZStack {
+         BonsaiColor.back
+            .ignoresSafeArea()
+         List {
+            HStack {
+               Image("undraw_japan_ubgk 1")
+                  .resizable()
+                  .scaledToFill()
+                  .clipped()
+                  .padding([.leading, .trailing], 73)
             }
-            .onAppear {
-                UITableView.appearance().showsVerticalScrollIndicator = false
+            .listRowBackground(BonsaiColor.back)
+            .listRowSeparator(.hidden)
+            .padding(.bottom, 12)
+            .padding(.top, 20)
+            HStack(alignment: .center) {
+               Spacer()
+               Text("Choose your plan")
+                  .font(.system(size: 28))
+                  .bold()
+                  .foregroundColor(BonsaiColor.purple7)
+               Spacer()
             }
-            .listStyle(PlainListStyle())
-            .edgesIgnoringSafeArea([.bottom, .leading, .trailing])
-        }
-    }
+            .listRowBackground(BonsaiColor.back)
+            .padding(.bottom, 12)
+            ForEach(0..<4) { index in
+               SubscriptionCell(subscription: mockSubs[index], id: id)
+                  .listRowSeparator(.hidden)
+                  .onTapGesture {
+                     let subscription = mockSubs[index]
+                     id = subscription.id
+                  }
+            }
+            .listRowBackground(BonsaiColor.back)
+            
+            
+            let termsOfServiceUrl = "https://duckduckgo.com"
+            let termsOfServicelink = "[Terms of Service](\(termsOfServiceUrl))"
+            
+            let privacyPolicyUrl = "https://duckduckgo.com"
+            let privacyPolicylink = "[Privacy Policy](\(privacyPolicyUrl))"
+            
+            Group {
+               Text("By subscribing you agree to our ") +
+               Text(.init(termsOfServicelink)).foregroundColor(BonsaiColor.secondary) +
+               Text(" and ") +
+               Text(.init(privacyPolicylink)).foregroundColor(BonsaiColor.secondary) +
+               Text(".")
+            }
+            .font(.system(size: 12))
+            .lineLimit(3)
+            .listRowSeparator(.hidden)
+            .listRowBackground(BonsaiColor.back)
+            
+            Button {
+               
+            } label: {
+               HStack(alignment: .center) {
+                  Text("Restore Purchases")
+                     .font(.system(size: 12))
+                     .foregroundColor(BonsaiColor.secondary)
+                  Spacer()
+               }
+            }
+            .listRowSeparator(.hidden)
+            .listRowBackground(BonsaiColor.back)
+            
+            HStack(alignment: .center) {
+               Spacer()
+               ZStack {
+                  RoundedRectangle(cornerRadius: 13)
+                     .frame(width: 192, height: 48)
+                     .foregroundColor(BonsaiColor.mainPurple)
+                  Text("Get all features")
+                     .foregroundColor(BonsaiColor.card)
+                     .font(.system(size: 17))
+                     .bold()
+               }
+               Spacer()
+            }
+            .padding(.bottom, 30)
+            .listRowSeparator(.hidden)
+            .listRowBackground(BonsaiColor.back)
+         }
+         .onAppear {
+            UITableView.appearance().showsVerticalScrollIndicator = false
+         }
+         .listStyle(PlainListStyle())
+         .edgesIgnoringSafeArea([.bottom, .leading, .trailing])
+      }
+   }
 }
 
 
 struct Subscriptions_Previews: PreviewProvider {
-    static var previews: some View {
-        Subscriptions()
-    }
+   static var previews: some View {
+      Subscriptions()
+   }
 }
