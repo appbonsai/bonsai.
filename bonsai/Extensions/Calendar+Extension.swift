0faec0704abfbf2aad10329efec2fe42c5a7198c
Maksim Alekseev <mrvalter566@gmail.com>
2022-10-26 21:01:47 +0700

    Add changes from BS-25

---
+++ bonsai/Extensions/Calendar+Extension.swift
@@ -0,0 +1,35 @@
+//
+//  Calendar+Extension.swift
+//  bonsai
+//
+//  Created by Максим Алексеев  on 26.07.2022.
+//
+
+import Foundation
+
+extension Date {
+    var startOfDay: Date {
+        return Calendar.current.startOfDay(for: self)
+    }
+    
+    var startOfMonth: Date {
+        let calendar = Calendar(identifier: .gregorian)
+        let components = calendar.dateComponents([.year, .month], from: self)
+        
+        return  calendar.date(from: components)!
+    }
+    
+    var endOfDay: Date {
+        var components = DateComponents()
+        components.day = 1
+        components.second = -1
+        return Calendar.current.date(byAdding: components, to: startOfDay)!
+    }
+    
+    var endOfMonth: Date {
+        var components = DateComponents()
+        components.month = 1
+        components.second = -1
+        return Calendar(identifier: .gregorian).date(byAdding: components, to: startOfMonth)!
+    }
+}
