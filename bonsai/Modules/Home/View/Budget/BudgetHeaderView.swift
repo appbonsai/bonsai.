1fb099732c8a0daef3887233ff702aadfe01e1dd
Valter <38043698+Valter4578@users.noreply.github.com>
2022-11-01 21:08:26 +0700

    [BS-135] Budget widget content  (#140)
    * Write functionality to calculate progress bar precentage for budget widget
    
    * Add functionality to show details for budget widget(total money spent and budget amount)

--- bonsai/Modules/Home/View/Budget/BudgetHeaderView.swift
+++ bonsai/Modules/Home/View/Budget/BudgetHeaderView.swift
@@ -8,46 +8,61 @@
 import SwiftUI
 
 struct BudgetHeaderView: View {
-
-   @Binding var progress: Double
-
-   var body: some View {
-      ZStack {
-         BonsaiColor.newPurple
-            .opacity(0.8)
-
-         HStack(spacing: 16) {
-
-            CircularProgressView(progress: progress)
-               .foregroundColor(BonsaiColor.text)
-               .frame(width: 75, height: 75)
-               .padding(.leading, 20)
-
-            VStack(alignment: .leading) {
-               Text("Money Spent")
-                  .font(BonsaiFont.body_17)
-                  .fontWeight(.regular)
-                  .foregroundColor(BonsaiColor.text)
-
-               Text("$14.00")
-                  .font(BonsaiFont.title_28)
-                  .foregroundColor(.white)
-               Text("out of $28")
-                  .font(BonsaiFont.body_15)
-                  .fontWeight(.regular)
-                  .foregroundColor(BonsaiColor.text)
-
+    @EnvironmentObject var budgetService: BudgetService
+    @FetchRequest(sortDescriptors: [SortDescriptor(\.date)]) var transactions: FetchedResults<Transaction>
+    
+    func getPercentage() -> Double {
+         getTotalMoneySpent() * 100.0 / getTotalBudgetAmount() / 100.0
+    }
+    
+    func getTotalMoneySpent() -> Double {
+        let allAmount = transactions.map { $0.amount }
+        return budgetService.getTotalMoneySpent(with: allAmount).doubleValue
+    }
+    
+    func getTotalBudgetAmount() -> Double {
+        budgetService.getTotalBudget().doubleValue
+    }
+    
+    @Binding var progress: Double
+    
+    var body: some View {
+        ZStack {
+            BonsaiColor.newPurple
+                .opacity(0.8)
+            
+            HStack(spacing: 16) {
+                
+                CircularProgressView(progress: getPercentage())
+                    .foregroundColor(BonsaiColor.text)
+                    .frame(width: 75, height: 75)
+                    .padding(.leading, 20)
+                
+                VStack(alignment: .leading) {
+                    Text("Money Spent")
+                        .font(BonsaiFont.body_17)
+                        .fontWeight(.regular)
+                        .foregroundColor(BonsaiColor.text)
+                    
+                    Text("$ \(getTotalMoneySpent(), specifier: "%.2f")")
+                        .font(BonsaiFont.title_28)
+                        .foregroundColor(.white)
+                    Text("out of $\(getTotalBudgetAmount(), specifier: "%.0f")")
+                        .font(BonsaiFont.body_15)
+                        .fontWeight(.regular)
+                        .foregroundColor(BonsaiColor.text)
+                    
+                }
+                
+                Spacer()
             }
-
-            Spacer()
-         }
-         .padding(.vertical, 28)
-      }
-   }
+            .padding(.vertical, 28)
+        }
+    }
 }
 
 struct BudgetHeaderView_Previews: PreviewProvider {
-   static var previews: some View {
-      BudgetHeaderView(progress: .constant(0.5))
-   }
+    static var previews: some View {
+        BudgetHeaderView(progress: .constant(0.5))
+    }
 }
