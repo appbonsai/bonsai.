6b0963ffd0f702d167228d5e1cad6ff0e0e56ed3
Maksim Alekseev <mrvalter566@gmail.com>
2022-11-12 22:53:34 +0700

    Changes in GifImage

--- bonsai/Views/GifImage.swift
+++ bonsai/Views/GifImage.swift
@@ -13,46 +13,50 @@ import WebKit
 
 struct GifImage: UIViewRepresentable {
     private let name: String
-
+    
     init(_ name: String) {
         self.name = name
     }
-
+    
     func makeUIView(context: Context) -> WKWebView {
         let webView = WKWebView()
         webView.isOpaque = false
         webView.backgroundColor = .clear
         webView.scrollView.backgroundColor = .clear
-       webView.isUserInteractionEnabled = false
-              
-       if let url = Bundle.main.url(forResource: name, withExtension: "png") {
-          let data = try! Data(contentsOf: url)
-          webView.load(
-            data,
-            mimeType: "image/gif",
-            characterEncodingName: "UTF-8",
-            baseURL: url.deletingLastPathComponent()
-          )
-          webView.scrollView.isScrollEnabled = false
-          return webView
-       } else {
-          let url = Bundle.main.url(forResource: name, withExtension: "gif")!
-          let data = try! Data(contentsOf: url)
-          webView.load(
-            data,
-            mimeType: "image/gif",
-            characterEncodingName: "UTF-8",
-            baseURL: url.deletingLastPathComponent()
-          )
-          webView.scrollView.isScrollEnabled = false
-          return webView
-       }
+        webView.isUserInteractionEnabled = false
+        webView.scrollView.isScrollEnabled = false
+        loadGif(webView: webView)
+        return webView
     }
-
+    
     func updateUIView(_ uiView: WKWebView, context: Context) {
+        URLCache.shared.removeAllCachedResponses()
+        URLCache.shared.diskCapacity = 0
+        URLCache.shared.memoryCapacity = 0
+        loadGif(webView: uiView)
         uiView.reload()
     }
-
+    
+    private func loadGif(webView: WKWebView) {
+        if let url = Bundle.main.url(forResource: name, withExtension: "png") {
+            let data = try! Data(contentsOf: url)
+            webView.load(
+                data,
+                mimeType: "image/gif",
+                characterEncodingName: "UTF-8",
+                baseURL: url//.deletingLastPathComponent()
+            )
+        } else {
+            let url = Bundle.main.url(forResource: name, withExtension: "gif")!
+            let data = try! Data(contentsOf: url)
+            webView.load(
+                data,
+                mimeType: "image/gif",
+                characterEncodingName: "UTF-8",
+                baseURL: url//.deletingLastPathComponent()
+            )
+        }
+    }
 }
 
 
