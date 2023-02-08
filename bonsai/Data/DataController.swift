f9b928fa8d18e279f83e357ee189cac5fe846c25
antuan.khoanh <antonhoang@Antons-MacBook-Pro.local>
2023-02-08 02:40:00 +0100

    fix icloud sync

--- bonsai/Data/DataController.swift
+++ bonsai/Data/DataController.swift
@@ -7,23 +7,38 @@
 
 import Foundation
 import CoreData
+import CloudKit
 
 final class DataController: ObservableObject {
     
     static let sharedInstance = DataController()
-    let container = NSPersistentCloudKitContainer(name: "Core")
-    private init() {
+    
+    lazy var container: NSPersistentContainer = {
+        let container = NSPersistentCloudKitContainer(name: "Core")
         let description = NSPersistentStoreDescription()
         description.cloudKitContainerOptions = NSPersistentCloudKitContainerOptions(containerIdentifier: "iCloud.com.bonsai.tree.app")
         description.shouldInferMappingModelAutomatically = true
         description.shouldMigrateStoreAutomatically = true
+        description.url = FileManager
+            .default
+            .urls(
+                for: .documentDirectory,
+                in: .userDomainMask
+            )
+            .first!
+            .appendingPathComponent("LocalStore.sqlite")
         container.persistentStoreDescriptions = [description]
-        container.loadPersistentStores { description, error in
-            if let error = error {
-                fatalError("Core Data failed to load: \(error.localizedDescription)")
+        container.loadPersistentStores { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
             }
         }
         container.viewContext.automaticallyMergesChangesFromParent = true
+        return container
+    }()
+    
+    private init() {
+//        container.viewContext.automaticallyMergesChangesFromParent = true
 #if DEBUG
         //      do {
         //         // Use the container to initialize the development schema.
